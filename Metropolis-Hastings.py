dc9e4cf9b328fdf027a77a04995162da4fbf6a6c
chriswang <wangming_0304@163.com>
2017-01-12 13:58:19 +0800

    Create Metropolis-Hastings.py

---
+++ Metropolis-Hastings.py
@@ -0,0 +1,37 @@
+# coding=utf-8
+
+import numpy as np
+import scipy.special as ss
+import matplotlib.pyplot as plt
+
+
+def beta_s(x, a, b):
+    return x ** (a - 1) * (1 - x) ** (b - 1)
+
+
+def beta(x, a, b):
+    return beta_s(x, a, b) / ss.beta(a, b)
+
+
+def plot_mcmc(a, b):
+    cur = np.random.rand()
+    states = [cur]
+    for i in range(10 ** 5):
+        next = u = np.random.rand()
+        # Metropolis-Hastings, 加大接受率α(xt,y)，避免迭代原地不动
+        if u < np.min((beta_s(next, a, b) / beta_s(cur, a, b), 1)):
+            states.append(next)
+            cur = next
+    x = np.arange(0, 1, .01)
+    plt.figure(figsize=(10, 5))
+    plt.plot(x, beta(x, a, b), lw=2, label='real dist: a={}, b={}'.format(a, b))
+    # 去掉前面burn-in阶段的值
+    plt.hist(states[-100:], 25, normed=True, label='simu mcmc: a={}, b={}'.format(a, b))
+    plt.show()
+
+
+if __name__ == '__main__':
+    # print np.random.rand()
+    plot_mcmc(0.1, 0.1)
+    plot_mcmc(1, 1)
+    plot_mcmc(4, 10)
